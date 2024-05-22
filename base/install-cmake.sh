1719712c9d858669579a34f38a3b7e39b0274f4b
Dalton Messmer <messmer.dalton@gmail.com>
2024-05-22 02:00:19 -0400

    Fix some dependencies on Ubuntu 24.04

--- base/install-cmake.sh
+++ base/install-cmake.sh
@@ -3,13 +3,6 @@
 # Kitware APT repo for latest CMake --- See: https://apt.kitware.com/
 # As of 5/21/2024, only focal and jammy are supported
 if [ "$UBUNTU_VERSION" = "20.04" ] || [ "$UBUNTU_VERSION" = "22.04" ]; then
-    apt-get update && apt-get install -y    \
-        --no-install-recommends             \
-            ca-certificates                 \
-            gpg                             \
-            lsb-release                     \
-            wget                            \
-        && rm -rf /var/lib/apt/lists/*
     test -f /usr/share/doc/kitware-archive-keyring/copyright || \
     wget -O - https://apt.kitware.com/keys/kitware-archive-latest.asc 2>/dev/null \
         | gpg --dearmor - | tee /usr/share/keyrings/kitware-archive-keyring.gpg >/dev/null
